9b87767382a3983166988d18555a8795bb1703cd
Rorisang Putu <rorisangputu@gmail.com>
2025-07-16 00:02:20 +0200

    importing bout component to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import About from "@/components/home/About";
 import Hero from "@/components/home/Hero";
 import Image from "next/image";
 
@@ -5,6 +6,7 @@ export default function Home() {
   return (
     <div>
       <Hero />
+      <About />
     </div>
   );
 }
