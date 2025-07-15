15dc7c8b98195e1cbf0d7012f15e8054da378356
Rorisang Putu <rorisangputu@gmail.com>
2025-07-15 18:50:12 +0200

    starting hero content

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -3,7 +3,19 @@ import React from "react";
 type Props = {};
 
 const Hero = (props: Props) => {
-  return <div>Hero</div>;
+  return (
+    <div className="h-[150vh] w-full py-5">
+      <div className="w-[90%] mx-auto">
+        <div className="">
+          <p>
+            Bringing ideas to life with creativity and a fresh perspective...
+          </p>
+          <div></div>
+        </div>
+        <div></div>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
