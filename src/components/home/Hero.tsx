a18bf9ce3e471af280fdea086d3013a4da3a357f
Rorisang Putu <rorisangputu@gmail.com>
2025-07-15 23:09:32 +0200

    removed image, fixed styling

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -11,21 +11,14 @@ const Hero = (props: Props) => {
     <div className="w-full py-5">
       <div className="mx-auto">
         <div className="">
-          <div className="w-[90%] mx-auto py-5 text-[#0e0e0e]">
+          <div className="w-[90%] mx-auto pt-5 text-[#0e0e0e]">
             <p className="text-4xl">
               Bringing ideas to life with creativity and a fresh perspective...
             </p>
           </div>
 
           {/* Image Container - must be relative and have defined height */}
-          <div className="relative h-[45vh] w-full overflow-hidden my-5">
-            {/* <Image
-              src={img}
-              fill
-              alt="hero-image"
-              className="object-cover"
-              priority
-            /> */}
+          <div className="flex items-center h-[400px] relative  w-full overflow-hidden   my-5">
             <HeroTextAnimation />
           </div>
           <div className="w-[90%] mx-auto mb-4 text-[#0e0e0e]">
