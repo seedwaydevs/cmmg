85250fbcca882b4c87bfb9c6686a8f0abf9f418e
Rorisang Putu <rorisangputu@gmail.com>
2025-07-15 21:33:36 +0200

    removing animation to its own component

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -1,18 +1,39 @@
+"use client";
+import Image from "next/image";
 import React from "react";
+import img from "../../../public/prodco.jpg";
+import HeroTextAnimation from "./HeroTextAnimation";
 
 type Props = {};
 
 const Hero = (props: Props) => {
   return (
-    <div className="h-[150vh] w-full py-5">
-      <div className="w-[90%] mx-auto">
+    <div className="w-full py-5">
+      <div className="mx-auto">
         <div className="">
-          <p>
-            Bringing ideas to life with creativity and a fresh perspective...
-          </p>
-          <div></div>
+          <div className="w-[90%] mx-auto py-5 text-[#0e0e0e]">
+            <p className="text-4xl">
+              Bringing ideas to life with creativity and a fresh perspective...
+            </p>
+          </div>
+
+          {/* Image Container - must be relative and have defined height */}
+          <div className="relative h-[45vh] w-full overflow-hidden my-5">
+            {/* <Image
+              src={img}
+              fill
+              alt="hero-image"
+              className="object-cover"
+              priority
+            /> */}
+            <HeroTextAnimation />
+          </div>
+          <div className="w-[90%] mx-auto mb-4 text-[#0e0e0e]">
+            <p className="text-4xl">
+              ...making sure every project looks and sounds just right.
+            </p>
+          </div>
         </div>
-        <div></div>
       </div>
     </div>
   );
