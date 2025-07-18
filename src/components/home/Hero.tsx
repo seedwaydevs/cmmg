951332f3535a450dadf85529260ebc9a6294f77e
Rorisang Putu <rorisangputu@gmail.com>
2025-07-18 11:42:05 +0200

    changes

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -3,6 +3,7 @@
 import React from "react";
 import HeroTextAnimation from "./HeroTextAnimation";
 import { Hanken_Grotesk } from "next/font/google";
+import Link from "next/link";
 
 type Props = {};
 
@@ -16,19 +17,25 @@ const Hero = (props: Props) => {
     <div className="w-full border">
       <div className="mx-auto md:w-[90%]">
         <div className="flex flex-col md:flex-row md:items-center">
-          <div className="w-[90%] lg:w-full mx-auto pt-5 text-[#0e0e0e] md:flex-1">
+          <div className="w-[90%] lg:w-full mx-auto py-16 text-[#0e0e0e] md:flex-1 space-y-8">
             <p
-              className={`${hanken.className} font-medium text-6xl tracking-tighter max-w-lg`}
+              className={`${hanken.className} font-medium text-4xl tracking-tighter max-w-lg`}
             >
               Bringing{" "}
-              <span className=" p-2 font-serif italic text-6xl">ideas</span> to
+              <span className=" p-2 font-serif italic text-4xl">ideas</span> to
               life with{" "}
-              <span className="p-2 font-serif italic text-6xl">creativity</span>{" "}
+              <span className="p-2 font-serif italic text-4xl">creativity</span>{" "}
               and a fresh{" "}
-              <span className="p-2 font-serif italic text-6xl">
+              <span className="p-2 font-serif italic text-4xl">
                 perspective
               </span>{" "}
             </p>
+            <Link
+              href={"/"}
+              className="bg-black text-white py-2 px-3 rounded-2xl cursor-pointer"
+            >
+              Learn More
+            </Link>
           </div>
 
           {/* Image Container - must be relative and have defined height */}
