8d656fcab2fa44ef037e0d62037f48c549a6e899
Rorisang Putu <rorisangputu@gmail.com>
2025-07-18 11:42:16 +0200

    adding new lib component

---
+++ src/components/home/Library.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+type Props = {};
+
+const Library = (props: Props) => {
+  return (
+    <div className="w-full h-screen">
+          <div className="w-[90%] mx-auto">
+              
+      </div>
+    </div>
+  );
+};
+
+export default Library;
