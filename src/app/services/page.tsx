23e8960170c20add6846f6c9be2df4b8a1be0f45
Rorisang Putu <rorisangputu@gmail.com>
2025-07-21 14:08:25 +0200

    changes

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -2,7 +2,9 @@ import { Anton, Hanken_Grotesk } from "next/font/google";
 import Image from "next/image";
 import React from "react";
 import { GoDotFill } from "react-icons/go";
-import image from "../../../public/prodco.jpg";
+import image from "../../../public/film.jpg";
+import content from "../../../public/content-dev.jpg";
+import music from "../../../public/music-prod.jpg";
 import LibraryImageGrid from "@/components/home/components/LibraryImageGrid";
 const antonFont = Anton({ subsets: ["latin"], weight: "400" });
 const hanken = Hanken_Grotesk({
@@ -46,8 +48,8 @@ const services = [
 const Services = () => {
   return (
     <div className="text-[#0e0e0e] w-full py-16 ">
-      <div className="w-[90%] mx-auto">
-        <div className="flex items-center gap-2 pb-4">
+      <div className="w-[90%] mx-auto h-[30vh] lg:h-[45vh] flex flex-col justify-center">
+        <div className="flex items-center gap-2 ">
           <GoDotFill className="h-6 w-6" />
           <p className={`${hanken.className} text-lg font-semibold`}>
             Our services
@@ -65,7 +67,7 @@ const Services = () => {
           </p>
         </div>
       </div>
-      <div className="py-16">
+      <div className="py-10">
         {/* Video */}
         <div className="bg-[#0e0e0e] py-10 text-white">
           <div className="w-[90%] mx-auto">
@@ -81,8 +83,11 @@ const Services = () => {
                   video content with cinematic precision and storytelling
                   excellence.
                 </p>
+                <p className="py-2 px-3 rounded-xl border border-white lg:w-[10vw] text-center">
+                  Contact Us
+                </p>
               </div>
-              <div className="w-full h-[50vh] flex-1">
+              <div className="w-full h-[70vh] flex-1">
                 <Image
                   src={image}
                   alt="image"
@@ -108,7 +113,7 @@ const Services = () => {
                   campaigns.
                 </p>
               </div>
-              <div className="w-full h-[75vh] flex-1">
+              <div className="w-full h-[95vh] flex-1">
                 <LibraryImageGrid />
               </div>
             </div>
@@ -128,10 +133,43 @@ const Services = () => {
                   We help brands plan, produce, and distribute content that
                   connects — from social media reels to full-scale campaigns.
                 </p>
+                <p className="py-2 px-3 rounded-xl border border-white text-white lg:w-[7vw] text-center">
+                  Contact
+                </p>
               </div>
-              <div className="w-full h-[50vh] flex-1">
+              <div className="w-full h-[70vh] flex-1">
                 <Image
-                  src={image}
+                  src={content}
+                  alt="image"
+                  className="w-full h-full object-cover"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* Music Composition & Scoring */}
+        <div className="py-10 text-[#0e0e0e]">
+          <div className="w-[90%] mx-auto">
+            <div className="flex flex-col lg:justify-center lg:items-center lg:flex-row space-y-10">
+              <div className="flex flex-col lg:justify-center flex-1 space-y-5">
+                <h1
+                  className={`${antonFont.className} text-7xl uppercase tracking-wider max-w-md leading-20`}
+                >
+                  Studio
+                </h1>
+                <p className="max-w-md text-xl">
+                  Our award-winning composers create custom scores and
+                  soundtracks tailored to elevate your story and captivate your
+                  audience.
+                </p>
+
+                <p className="py-2 px-3 rounded-xl border border-black lg:w-[7vw] text-center">
+                  Book
+                </p>
+              </div>
+              <div className="w-full h-[70vh] flex-1">
+                <Image
+                  src={music}
                   alt="image"
                   className="w-full h-full object-cover"
                 />
