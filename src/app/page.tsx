e8ea58c57dd7f8b7831fae74e8cb45a612defebb
Rorisang Putu <rorisangputu@gmail.com>
2025-07-23 00:58:59 +0200

    removing text animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import ContactCTA from "@/components/about/ContactCTA";
+
 import Explore from "@/components/home/Explore";
 import Hero from "@/components/home/Hero";
 import TrustCarousel from "@/components/home/TrustCarousel";
