7269cc5076564a50312e244843ac479218f5ad82
Rorisang Putu <rorisangputu@gmail.com>
2025-07-22 19:29:27 +0200

    updating explore

--- src/components/home/Explore.tsx
+++ src/components/home/Explore.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import React from "react";
 import { TiArrowSortedUp } from "react-icons/ti";
 import collage from "../../../public/CoverCollage.png";
+import Link from "next/link";
 type Props = {};
 const hanken = Hanken_Grotesk({
   subsets: ["latin"],
@@ -17,12 +18,22 @@ const Explore = (props: Props) => {
             <TiArrowSortedUp className="text-orange-600 h-7 w-7 lg:h-10 lg:w-10" />
             <p className="text-xs uppercase lg:text-lg">[ Explore ]</p>
           </div>
-          <div className="flex flex-col lg:items-center lg:flex-row space-y-7">
-            <h1
-              className={`${hanken.className} font-bold text-5xl lg:text-8xl flex-1`}
-            >
-              CMMG Production Music Library
-            </h1>
+          <div className="flex flex-col lg:items-center lg:justify-between lg:flex-row space-y-7 pb-5">
+            <div className="space-y-7">
+              <h1
+                className={`${hanken.className} font-bold text-5xl lg:text-7xl lg:max-w-lg flex-1`}
+              >
+                CMMG Production Music Library
+              </h1>
+              <p className="lg:text-xl lg:max-w-2xl">
+                Explore a curated library of high-impact production music —
+                designed to bring your film, TV, or digital project to life with
+                unforgettable sound.
+              </p>
+              <Link href={"/"} className="bg-orange-600 py-2 px-3 text-white">
+                Explore
+              </Link>
+            </div>
             <div className="h-[30vh] lg:h-[30vh] bg-black">
               <Image
                 src={collage}
@@ -31,6 +42,21 @@ const Explore = (props: Props) => {
               />
             </div>
           </div>
+          <hr className="" />
+          <div className="py-5 lg:py-10">
+            <p
+              className={`${hanken.className} text-4xl md:text-5xl lg:text-6xl tracking-tight text-gray-500 font-bold `}
+            >
+              <span className="text-black">
+                We don’t follow trends — we set the tone.{" "}
+              </span>{" "}
+              <span>
+                We’re a content and music powerhouse. Built for impact.
+                Engineered to resonate.
+              </span>
+            </p>
+          </div>
+          <hr className="" />
         </div>
       </div>
     </div>
