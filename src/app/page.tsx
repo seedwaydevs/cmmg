dca9a0cf1f66013eb082abda3313362fc5fcbc28
Rorisang Putu <rorisangputu@gmail.com>
2025-07-22 16:48:09 +0200

    importing explore component to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import Explore from "@/components/home/Explore";
 import Hero from "@/components/home/Hero";
+import TrustCarousel from "@/components/home/TrustCarousel";
 
 export default function Home() {
   return (
     <div>
       <Hero />
+      <TrustCarousel />
+      <Explore />
     </div>
   );
 }
