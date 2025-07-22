39244a45a52c4f4738b3c39cb375874bc0d46c5b
Rorisang Putu <rorisangputu@gmail.com>
2025-07-22 15:12:39 +0200

    changing text layout and bg

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 import HeroTextAnimation from "./components/HeroTextAnimation";
-import { Hanken_Grotesk } from "next/font/google";
+import { Hanken_Grotesk, Schibsted_Grotesk } from "next/font/google";
 import Link from "next/link";
 
 type Props = {};
@@ -12,43 +12,41 @@ const hanken = Hanken_Grotesk({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
 
+const sted = Schibsted_Grotesk({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700", "800", "900"],
+});
+
 const Hero = (props: Props) => {
   return (
-    <div className="w-full ">
-      <div className="mx-auto md:w-[90%]">
-        <div className="flex flex-col md:flex-row md:items-center">
-          <div className="w-[90%] lg:w-full mx-auto py-16 text-[#0e0e0e] md:flex-1 space-y-8">
-            <p
-              className={`${hanken.className} font-medium text-4xl tracking-tighter max-w-lg`}
-            >
-              Bringing{" "}
-              <span className=" p-2 font-serif italic text-4xl">ideas</span> to
-              life with{" "}
-              <span className="py-2 font-serif italic text-4xl">
-                creativity
-              </span>{" "}
-              and a fresh{" "}
-              <span className="py-2 font-serif italic text-4xl">
-                perspective
-              </span>{" "}
-            </p>
-            <div className="flex">
-              <Link
-                href={"/"}
-                className="bg-black text-white py-2 px-3 rounded-2xl cursor-pointer"
-              >
-                Learn More
-              </Link>
+    <>
+      {/* <div className="h-[80px] border"></div> */}
+      <div className="py-16 w-full bg-[#0e0e0e] text-white h-screen">
+        <div className="w-[90%] md:w-[90%] mx-auto h-full">
+          <div className="flex flex-col justify-between h-full">
+            <div className="flex justify-end mt-16">
+              <p className="text-[16px] uppercase font-bold tracking-tighter">
+                [ Content and Music ]
+              </p>
             </div>
-          </div>
-
-          {/* Image Container - must be relative and have defined height */}
-          <div className="flex items-center  h-[400px] lg:h-[500px] md:flex-1  relative w-full overflow-hidden">
-            <HeroTextAnimation />
+            <div className={`${sted.className}`}>
+              <p className="text-lg font-medium max-w-xs md:max-w-sm">
+                Crafting original music libraries and immersive content for
+                film, television, and digital storytelling.
+              </p>
+              <h1
+                className={`${hanken.className} text-8xl font-extrabold py-7`}
+              >
+                CMMG<span className="text-orange-600 text-[50px]">.</span>
+              </h1>
+              <p className="text-[16px] uppercase font-bold tracking-tighter">
+                [ Media Group ]
+              </p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
