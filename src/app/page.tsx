e4326a954b287184c398d3dea34ac06307788246
Rorisang Putu <rorisangputu@gmail.com>
2025-07-23 00:00:15 +0200

    importing contact component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import ContactCTA from "@/components/about/ContactCTA";
 import Explore from "@/components/home/Explore";
 import Hero from "@/components/home/Hero";
 import TrustCarousel from "@/components/home/TrustCarousel";
@@ -10,6 +11,7 @@ export default function Home() {
       <TrustCarousel />
       <Explore />
       <WhatWeDo />
+      <ContactCTA />
     </div>
   );
 }
