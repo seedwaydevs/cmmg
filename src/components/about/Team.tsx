be017243ecc0e9a44fb97b650297b31665f753e1
Rorisang Putu <rorisangputu@gmail.com>
2025-07-23 23:28:40 +0200

    updating spacing

--- src/components/about/Team.tsx
+++ src/components/about/Team.tsx
@@ -1,4 +1,4 @@
-import { Anton, Hanken_Grotesk } from "next/font/google";
+import { Anton, Hanken_Grotesk, Inter_Tight } from "next/font/google";
 import Image from "next/image";
 import React from "react";
 import { GoDotFill } from "react-icons/go";
@@ -8,6 +8,7 @@ import { FaFacebookF } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 import TeamCard from "./TeamCard";
+import { TiArrowSortedUp } from "react-icons/ti";
 
 type Props = {};
 
@@ -16,19 +17,21 @@ const hanken = Hanken_Grotesk({
   subsets: ["latin"],
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
+const inter = Inter_Tight({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
 const Team = (props: Props) => {
   return (
     <div className="w-full py-10">
       <div className="flex flex-col">
-        <div className="flex items-center space-x-2">
-          <GoDotFill className="h-6 w-6" />
-          <p className={`${hanken.className} text-lg font-semibold`}>
-            Our Team
-          </p>
+        <div className="flex items-center">
+          <TiArrowSortedUp className="text-orange-600 h-7 w-7 lg:h-10 lg:w-10" />
+          <p className="text-xs uppercase lg:text-lg">[ Our Team ]</p>
         </div>
         <div className="py-5">
           <h1
-            className={`${antonFont.className} text-7xl md:text-8xl md:max-w-xl uppercase`}
+            className={`${hanken.className} text-5xl sm:text-6xl md:text-7xl sm:max-w-lg md:max-w-xl uppercase font-extrabold`}
           >
             Meet our talented team.
           </h1>
--- src/components/about/TeamCard.tsx
+++ src/components/about/TeamCard.tsx
@@ -14,7 +14,7 @@ const TeamCard = ({ image, name, position, font }: Props) => {
   const [first, ...last] = name.split(" ");
   return (
     <div className="space-y-3">
-      <div className="h-80 sm:h-[450px] lg:h-[500px] rounded-xl overflow-hidden">
+      <div className="h-140 sm:h-[450px] lg:h-[600px] xl:h-[550px] rounded-xl overflow-hidden">
         <Image
           src={image}
           alt={name}
@@ -22,26 +22,28 @@ const TeamCard = ({ image, name, position, font }: Props) => {
         />
       </div>
 
-      <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between gap-2">
-        <p className={`${font.className} text-2xl sm:text-3xl uppercase`}>
-          <span className="font-serif italic lowercase">{first}</span>{" "}
-          {last.join(" ")}
-        </p>
+      <div className="py-2">
+        <div className="flex sm:flex-row sm:items-center justify-between gap-2">
+          <p className={`${font.className} text-2xl sm:text-3xl uppercase`}>
+            <span className="font-serif italic lowercase">{first}</span>{" "}
+            {last.join(" ")}
+          </p>
 
-        <div className="flex items-center space-x-3">
-          <Link href="/">
-            <FaFacebookF className="h-5 w-5 sm:h-6 sm:w-6" />
-          </Link>
-          <Link href="/">
-            <FaLinkedin className="h-5 w-5 sm:h-6 sm:w-6" />
-          </Link>
-          <Link href="/">
-            <FaXTwitter className="h-5 w-5 sm:h-6 sm:w-6" />
-          </Link>
+          <div className="flex items-center space-x-3">
+            <Link href="/">
+              <FaFacebookF className="h-5 w-5 sm:h-6 sm:w-6" />
+            </Link>
+            <Link href="/">
+              <FaLinkedin className="h-5 w-5 sm:h-6 sm:w-6" />
+            </Link>
+            <Link href="/">
+              <FaXTwitter className="h-5 w-5 sm:h-6 sm:w-6" />
+            </Link>
+          </div>
         </div>
-      </div>
 
-      <p className="text-gray-600">{position}</p>
+        <p className="text-gray-600">{position}</p>
+      </div>
     </div>
   );
 };
