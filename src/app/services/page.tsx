c6594801464c133bbcca380bc6eab2f467a7bbc4
Rorisang Putu <rorisangputu@gmail.com>
2025-07-24 20:17:31 +0200

    adding first component to services page

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,9 +1,14 @@
+import Hero from "@/components/services/Hero";
 import React from "react";
 
 type Props = {};
 
 const page = (props: Props) => {
-  return <div>Ola</div>;
+  return (
+    <div className="flex flex-col">
+      <Hero />
+    </div>
+  );
 };
 
 export default page;
