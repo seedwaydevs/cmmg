be015935b6a04c9592bd96ed5df7f7b6e3a76969
Rorisang Putu <rorisangputu@gmail.com>
2025-07-24 20:17:46 +0200

    creating hero component for services page

---
+++ src/components/services/Hero.tsx
@@ -0,0 +1,44 @@
+import { Hanken_Grotesk, Inter_Tight } from "next/font/google";
+import React from "react";
+import { TiArrowSortedUp } from "react-icons/ti";
+
+type Props = {};
+const hanken = Hanken_Grotesk({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
+const inter = Inter_Tight({
+  subsets: ["latin"],
+  weight: ["400", "500", "600"],
+});
+const Hero = (props: Props) => {
+  return (
+    <div className="bg-[#0e0e0e] h-[50vh] md:h-[60vh] border">
+      <div className="w-[95%] mx-auto">
+        <div className="mt-28 flex flex-col justify-center items-center text-white  md:py-5">
+          <div className="space-y-3 max-w-md md:max-w-lg lg:max-w-2xl">
+            <div className="flex items-center justify-center">
+              <p className="text-xs uppercase lg:text-lg">[ services ]</p>
+            </div>
+            <p
+              className={`${hanken.className} text-orange-600 font-extrabold tracking-tight text-4xl md:text-5xl lg:text-6xl text-center uppercase`}
+            >
+              built for production-grade impact.
+            </p>
+          </div>
+          <p
+            className={`${inter.className} text-center py-5 max-w-md md:max-w-lg lg:max-w-2xl md:text-lg`}
+          >
+            We offer end-to-end audio and visual production services — from
+            original compositions and final mix to studio hire, sound design,
+            and licensable music. Whether you're building a campaign, scoring a
+            film, or launching a podcast, we bring the technical precision and
+            creative energy your project needs.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
