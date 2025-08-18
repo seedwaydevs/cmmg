44b199d543cbf490ef875e63bd9886da59d045b1
Rorisang Putu <rorisangputu@gmail.com>
2025-08-18 11:15:23 +0200

    creating new photo gallery page with gallery component

---
+++ src/app/gallery/page.tsx
@@ -0,0 +1,14 @@
+import PhotoGallery from "@/components/gallery/Gallery";
+import React from "react";
+
+type Props = {};
+
+const page = (props: Props) => {
+  return (
+    <div>
+      <PhotoGallery />
+    </div>
+  );
+};
+
+export default page;
