2670f129dd7509729f9507d4db4fc27a0e266b28
Rorisang Putu <rorisangputu@gmail.com>
2025-08-18 12:28:54 +0200

    updating positioning

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,8 +12,8 @@ export default function Home() {
     <div>
       <Hero />
       <TrustCarousel />
-      <Latest />
       <Explore />
+      <Latest />
       <WhatWeDo />
       <MusicServices />
       <ContactCTA />
