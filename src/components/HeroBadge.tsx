05e308ab5ef9a4844c3d01ab9375b11acc4c329b
Rorisang Putu <rorisangputu@gmail.com>
2025-11-04 10:52:29 +0200

    creating new hero badge for consistent styling

---
+++ src/components/HeroBadge.tsx
@@ -0,0 +1,25 @@
+export default function HeroBadge({
+  content,
+  position,
+}: {
+  content: string;
+  position?: string;
+}) {
+  return (
+    <div
+      className={`flex  ${
+        position === "bottom"
+          ? "justify-start"
+          : "justify-end mt-20 sm:mt-24 xl:mt-24"
+      }`}
+    >
+      <div className="backdrop-blur-sm bg-white/5 px-4 py-2 rounded-lg border border-orange-400/20">
+        <p
+          className={`text-[14px] uppercase font-bold tracking-widest text-neutral-100`}
+        >
+          [ {content} ]
+        </p>
+      </div>
+    </div>
+  );
+}
