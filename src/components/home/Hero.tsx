27a75717dcbbfc46d22c23944876cf0a5900e2b7
Rorisang Putu <rorisangputu@gmail.com>
2025-12-03 23:57:32 +0200

    adding studio component to home page

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -52,7 +52,7 @@ const Hero = (props: Props) => {
   useEffect(() => {
     const timer = setInterval(() => {
       setCurrentSlide((prev) => (prev + 1) % slides.length);
-    }, 10000);
+    }, 15000);
 
     return () => clearInterval(timer);
   }, [slides.length]);
