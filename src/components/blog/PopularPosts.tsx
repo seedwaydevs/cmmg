56477dfbff3935bcf44ccfebd8e8086ff8e9cdca
Rorisang Putu <rorisangputu@gmail.com>
2026-03-04 13:55:32 +0200

    Remove deprecated blog components and related content

--- src/components/blog/PopularPosts.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-type Props = {};
-
-const PopularPosts = (props: Props) => {
-  return <div>PopularPosts</div>;
-};
-
-export default PopularPosts;
--- src/components/blog/TopCategories.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-type Props = {};
-
-const TopCategories = (props: Props) => {
-  return <div>TopCategories</div>;
-};
-
-export default TopCategories;
